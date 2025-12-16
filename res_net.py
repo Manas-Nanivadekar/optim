8c5ccaf9bc50bd63fa123baea292574ceb7fc9bd
Manas-Nanivadekar <manasnanivadekar@gmail.com>
2025-12-16 16:18:44 +0530

    fix: bug

--- res_net.py
+++ res_net.py
@@ -32,7 +32,7 @@ class Block(nn.Module):
     def forward(self, x):
         out = F.relu(self.bn1(self.conv1(x)))
         out = self.bn2(self.conv2(out))
-        out += self.shortcut
+        out += self.shortcut(x)
         out = F.relu(out)
         return out
 
